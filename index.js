fceaa4c754bc1d253f154617f5d773bacf98f96b
MOHAMMAD ALMOHAMMAD <neotaiki123@gmail.com>
2023-03-18 00:37:59 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+//first assignment with for loop
+
+function writeCards(names,event)
+{
+    const messages=[];
+    for (let i=0;i<names.length;i++)
+    {
+        messages[i]=`Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return messages;
+}
+
+//second assignment with while loop
+function countDown(number)
+{
+    while(number>=0)
+    {
+        console.log(number);
+        number--;
+    }
+}
